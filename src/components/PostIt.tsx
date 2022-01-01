0cb8a4d02ee0fcf03fbcde885d446b7179c24040
jheeju <jheeju.dev@gmail.com>
2022-01-01 20:25:56 +0900

    feat(add-post-it): PostIt 컴포넌트 스타일링

--- src/components/PostIt.tsx
+++ src/components/PostIt.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { HiMinusCircle } from 'react-icons/hi';
+import { RiCloseCircleFill } from 'react-icons/ri';
 interface IState {
   item: {
     id: number;
@@ -9,7 +10,18 @@ interface IState {
 }
 
 const PostIt: React.FC<IState> = ({ item }) => {
-  return <div className="postIt-wrapper">{item.content}</div>;
+  return (
+    <div className="postIt-wrapper">
+      <div className="postIt-title-bar">
+        {item.title}
+        <div className="icons">
+          <HiMinusCircle />
+          <RiCloseCircleFill />
+        </div>
+      </div>
+      <div className="postIt-content">{item.content}</div>
+    </div>
+  );
 };
 
 export default PostIt;
