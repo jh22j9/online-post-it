aa68f3eaa6d36eabf89e77ebbb2f9b46bf97e60b
jheeju <jheeju.dev@gmail.com>
2022-01-01 20:38:05 +0900

    feat(add-post-it): 더블클릭한 위치에 포스트잇 생성하기

--- src/components/PostIt.tsx
+++ src/components/PostIt.tsx
@@ -6,12 +6,20 @@ interface IState {
     id: number;
     title: string;
     content: string;
+    positions: number[];
   };
 }
 
 const PostIt: React.FC<IState> = ({ item }) => {
+  const [x, y] = item.positions;
+
+  const style = {
+    left: `${x}px`,
+    top: `${y}px`,
+  };
+
   return (
-    <div className="postIt-wrapper">
+    <div className="postIt-wrapper" style={style}>
       <div className="postIt-title-bar">
         {item.title}
         <div className="icons">
