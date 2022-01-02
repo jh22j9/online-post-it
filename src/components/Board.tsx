017eaea8afe36101aae4a8cd853962aa4c6d607b
jheeju <jheeju.dev@gmail.com>
2022-01-02 20:38:18 +0900

    mend

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PostIt from './PostIt';
+import { useSelector } from 'react-redux';
+import { State } from '../state';
 
 export interface IState {
   postItList: {
@@ -11,6 +13,9 @@ export interface IState {
 }
 
 const Board: React.FC = () => {
+  const { currentBoard } = useSelector((state: State) => state.postBoard);
+  console.log('currentBoard', currentBoard);
+
   const [postItList, setPostItList] = useState([{ id: 1, title: '', content: '', positions: [30, 30] }]);
 
   const handleDoubleClick = (e: React.MouseEvent<HTMLElement>) => {
@@ -29,7 +34,8 @@ const Board: React.FC = () => {
 
   return (
     <section className="board-wrapper">
-      <div className="board" onDoubleClick={handleDoubleClick}>
+      <div className="board-title">{}</div>
+      <div className="board-content" onDoubleClick={handleDoubleClick}>
         {postItList.map((postIt) => (
           <PostIt item={postIt} key={postIt.id} postItList={postItList} setPostItList={setPostItList} />
         ))}
--- src/state/reducers/boardReducer.ts
+++ src/state/reducers/boardReducer.ts
@@ -15,24 +15,28 @@ export interface BoardInterface {
 }
 
 export interface StateInterface {
-  currentBoardId: number;
+  currentBoard: BoardInterface;
   boardList: BoardInterface[];
   // boardList: BoardInterface[] | []
 }
 
+const boardList = [
+  { bId: 1, name: 'Board1', postItList: [] },
+  { bId: 2, name: 'Board2', postItList: [] },
+];
+
 const initialState: StateInterface = {
-  currentBoardId: 1,
-  boardList: [
-    { bId: 1, name: 'Board1', postItList: [] },
-    { bId: 2, name: 'Board2', postItList: [] },
-  ],
+  currentBoard: boardList[0],
+  boardList: [...boardList],
 };
 
 const reducer = (state: StateInterface = initialState, action: Action): StateInterface => {
   switch (action.type) {
     case ActionType.ADD_BOARD: {
       const newBoard = { bId: state.boardList.length + 1, name: 'New Board!', postItList: [] };
-      return { ...state, boardList: [...state.boardList, newBoard] };
+      state = { ...state, boardList: [...state.boardList, newBoard] };
+      state.currentBoard = newBoard;
+      return state;
     }
     case ActionType.ADD_POST_IT:
       return state;
