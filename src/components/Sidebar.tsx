6bec404a06529d8ece05c6231086f689681945a5
jheeju <jheeju.dev@gmail.com>
2022-01-02 20:16:31 +0900

    feat(add-board): Sidebar 스타일링 및 버튼 추가

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { State } from '../state';
+import { HiPlusSm } from 'react-icons/hi';
 
 const Sidebar: React.FC = () => {
+  const boardList = useSelector((state: State) => state.postBoard.boardList);
+
   return (
     <section className="sidebar-wrapper">
-      <div className="sidebar">
-        <ul>
-          <li>보드 1</li>
-          <li>보드 2</li>
-        </ul>
-      </div>
+      <ul>
+        {boardList.map((board) => (
+          <li key={board.bId}>{board.name}</li>
+        ))}
+      </ul>
+      <HiPlusSm className="add-icon" />
     </section>
   );
 };
